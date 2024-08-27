b3fc06683df651a1724727fb87086894279fd5ee
Thyago-ES <thyagorafael57@gmail.com>
2024-08-27 10:20:11 -0300

    feat: create contacts routes

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@
 import { Router } from "express";
 
 import customers from "./app/controllers/CustomersController";
+import contacts from "./app/controllers/ContactsController";
 
 const routes = new Router();
 
@@ -10,5 +11,11 @@ routes.get("/customers/:id", customers.show);
 routes.post("/customers", customers.create);
 routes.put("/customers/:id", customers.update);
 routes.delete("/customers/:id", customers.destroy);
+
+routes.get("/customers/:customerId/contacts", contacts.index);
+routes.get("/customers/:customerId/contacts/:id", contacts.show);
+routes.post("/customers/:customerId/contacts", contacts.create);
+routes.put("/customers/:customerId/contacts/:id", contacts.update);
+routes.delete("/customers/:customerId/contacts/:id", contacts.destroy);
 
 export default routes;
